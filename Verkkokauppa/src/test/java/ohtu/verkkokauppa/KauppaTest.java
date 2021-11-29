05e6f14af45dd9f204264c386c59a098d7b50fad
Joel6677 <joel.heusala@gmail.com>
2021-11-30 00:05:16 +0200

    4

--- Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -206,8 +206,9 @@ public class KauppaTest {
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
-        when(varasto.saldo(2)).thenReturn(40);
+        when(varasto.saldo(2)).thenReturn(0);
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 20));
+
         // sitten testattava kauppa 
         Kauppa k = new Kauppa(varasto, pankki, viite);
 
@@ -215,10 +216,11 @@ public class KauppaTest {
         k.aloitaAsiointi();
         k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
         k.lisaaKoriin(2);
-        k.poistaKorista(1);
+        k.poistaKorista(2);
         k.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
-        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(20));
+        verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(), eq(5));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 }
